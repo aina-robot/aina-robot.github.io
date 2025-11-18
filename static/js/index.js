f916674ec530b606d13d78bd6a7c1541a909e342
Irmak Guzey <irmakguzey@meta.com>
2025-11-17 21:35:23 -0500

    Added the task and method videos and changed the template to be similar to feel-the-force

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,6 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
-
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
-
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-    });
-
     var options = {
 			slidesToScroll: 1,
 			slidesToShow: 3,
@@ -57,22 +29,23 @@ $(document).ready(function() {
     		console.log(state);
     	});
     }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
-    bulmaSlider.attach();
-
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+	const tabs = document.querySelectorAll('#ftf-video-tabs li');
+	const contents = document.querySelectorAll('#ftf-video-display .video-content');
+  
+	tabs.forEach(tab => {
+	  tab.addEventListener('click', () => {
+		const targetId = tab.getAttribute('data-target');
+  
+		tabs.forEach(t => t.classList.remove('is-active'));
+  
+		contents.forEach(content => content.style.display = 'none');
+  
+		tab.classList.add('is-active');
+		document.getElementById(targetId).style.display = '';
+	  });
+	});
+  });
+  
